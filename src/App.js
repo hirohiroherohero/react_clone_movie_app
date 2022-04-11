c618cd1be1631807efae8a1ac11c9990aee2dcdb
hiro <70766495+hirohiroherohero@users.noreply.github.com>
2022-04-11 16:56:03 +0900

    useEffect, deps

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
+import React, { useEffect, useState } from 'react';
 import Button from './Button'
 import styles from './style.module.css'
 
 function App() {
+  const [couter, setValue] = useState(0)
+  const [keyworld, setKeyworld] = useState('')
+
+  const increase = () => {
+    setValue(prev => prev + 1)
+  }
+
+  const handleTextInput = (e) => {
+    setKeyworld(e.target.value)
+  }
+
+
+  useEffect(() => {
+    console.log('한번만 실행')
+  }, [])
+
+  useEffect(() => {
+    console.log('키워드 작성시에만 실행')
+  }, [keyworld])
+
+  useEffect(() => {
+    console.log('카운터에만 실행')
+  }, [couter])
+
+  useEffect(() => {
+    console.log('카운터와 키워드 작성시에 실행')
+  }, [keyworld, couter])
+
   return (
     <>
-      <h1 className={styles.title}>welcome</h1>
-      <Button text="click me!" />
+      <h1 className={styles.title}>{couter}</h1>
+      <Button text="click me!" clickEvent={increase}/>
+
+      <div>
+        <input type="text" value={keyworld} onChange={handleTextInput}/>
+      </div>
     </>
   );
 }
--- src/Button.js
+++ src/Button.js
@@ -1,7 +1,7 @@
 import styles from './style.module.css'
 
 const Button = (props) => {
-    return <button className={styles.btn}>{props.text}</button>
+    return <button className={styles.btn} onClick={props.clickEvent}>{props.text}</button>
 }
 
 export default Button
